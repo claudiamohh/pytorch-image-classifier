ce8e036ec13bf607782eab886c546dcb6a7839be
Claudia <claudiamhch@gmail.com>
2022-06-21 12:02:29 +0800

    new file:   MNIST.py

---
+++ datasets/MNIST.py
@@ -0,0 +1,28 @@
+    ''' MNIST Dataset '''
+
+from torchvision imports datasets
+from torchvision.transforms import ToTensor
+
+class MNISTDataset():
+    def __init__(self):
+        super().__init__()
+
+        self.transform = transforms.Compose(
+                [transforms.ToTensor(),
+                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+        self.trainset = datasets.MNIST(
+                root = 'data', train = True, transform = self.transform, download = True)
+
+        self.trainloader = torch.utils.data.DataLoader(
+                self.trainset, batch_size = 100, shuffle = True, num_workers = 1)
+
+        
+        self.testset = datasets.MNIST(
+                root = 'data', train = False, transform = self.transform)
+
+        self.testloader = torch.utils.data.DataLoader(
+                self.testset, batch_size = 100, shuffle = True, num_workers = 1)
+
+    def get_loaders():
+        return (self.trainloader, self.testloader)
