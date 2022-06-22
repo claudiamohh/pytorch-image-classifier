a57ca0621590f96f113bba1a8d74acdc4504fd0d
Claudia <claudiamhch@gmail.com>
2022-06-22 11:54:49 +0800

    CNNClassifier

--- models/CNNClassifier.py
+++ models/CNNClassifier.py
@@ -1,5 +1,10 @@
-    ''' Large CNN Model '''
-class CNNClassifier():
+''' Large CNN Model '''
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNNClassifier(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size = 3, padding = 1)
--- models/LinearClassifier.py
+++ models/LinearClassifier.py
@@ -1,8 +1,13 @@
-    ''' Linear CNN Model '''
-class LinearCNN():
+''' Linear CNN Model '''
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LinearCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(3 * 32 * 32, 120)      #input has to be image size to fit 
+        self.fc1 = nn.Linear(3*32*32, 120)      #input has to be image size to fit 
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
@@ -11,4 +16,4 @@ class LinearCNN():
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        return x 
+        return x
