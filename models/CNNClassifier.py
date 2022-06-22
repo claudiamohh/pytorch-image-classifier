0e26c3c724b780eba23707f96cf1e79b0a9e7f42
Claudia <claudiamhch@gmail.com>
2022-06-22 16:36:33 +0800

    Changed class name to 'CNN'

--- models/CNNClassifier.py
+++ models/CNNClassifier.py
@@ -6,7 +6,7 @@ import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CNNClassifier(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size = 3, padding = 1)
