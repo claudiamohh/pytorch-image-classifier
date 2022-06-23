cb85f36b9d17891486ed7072c5ddf52e8416e89f
Lik Xun Yuan <likyuan@handshakes.com.sg>
2022-06-23 18:24:25 +0800

    Fix model classes formatting

--- models/CNNClassifier.py
+++ models/CNNClassifier.py
@@ -1,5 +1,5 @@
-''' Large CNN Model with 6 convolution layers, pooling layers and 3 fully connected layers.
-First convolution layer takes in a channel of dimension 3 since the images are RGB, kernel size is 3*3. The output of this convolution layer is set to 32 channels which means it will extract 32 feature maps using 32 kernels. Padding size is set to 1 so that input and output dimensions are the same. Then it goes through ReLu activation followed by a max-pooling layer with kernel size of 2 and stride 2.'''
+""" Large CNN Model with 6 convolution layers, pooling layers and 3 fully connected layers.
+First convolution layer takes in a channel of dimension 3 since the images are RGB, kernel size is 3*3. The output of this convolution layer is set to 32 channels which means it will extract 32 feature maps using 32 kernels. Padding size is set to 1 so that input and output dimensions are the same. Then it goes through ReLu activation followed by a max-pooling layer with kernel size of 2 and stride 2."""
 
 import torch
 import torchvision
@@ -14,12 +14,12 @@ class CNN(nn.Module):
         self.channels = channels
         self.use_mnist = use_mnist
 
-        self.conv1 = nn.Conv2d(self.channels, 32, kernel_size = 3, padding = 1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size = 3, stride = 1, padding = 1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 1)
-        self.conv4 = nn.Conv2d(128, 128, kernel_size = 3, stride = 1, padding = 1)
-        self.conv5 = nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1)
-        self.conv6 = nn.Conv2d(256, 256, kernel_size = 3, stride = 1, padding = 1)
+        self.conv1 = nn.Conv2d(self.channels, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
 
         if self.use_mnist:
@@ -29,7 +29,7 @@ class CNN(nn.Module):
         self.fc2 = nn.Linear(1024, 512)
         self.fc3 = nn.Linear(512, 10)
 
-    def forward(self,x):
+    def forward(self, x):
         x = F.relu(self.conv1(x))
         x = self.pool(F.relu(self.conv2(x)))
         x = F.relu(self.conv3(x))
--- models/LinearClassifier.py
+++ models/LinearClassifier.py
@@ -1,17 +1,20 @@
-''' Linear Classifier CNN Model '''
+""" Linear Classifier CNN Model """
 import torch
 import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Linear(nn.Module):
     def __init__(self, channels, size):
         super().__init__()
-        self.fc1 = nn.Linear(channels*size*size, 120)      #input has to be image size to fit 
+        self.fc1 = nn.Linear(
+            channels * size * size, 120
+        )  # input has to be image size to fit
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
-    def forward(self,x):
+    def forward(self, x):
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
