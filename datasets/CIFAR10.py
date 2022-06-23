6881b26fb13080810c7c2ab6fe9d7daaefbadd67
Lik Xun Yuan <likyuan@handshakes.com.sg>
2022-06-23 18:24:55 +0800

    Fix dataset classes formatting

--- datasets/CIFAR10.py
+++ datasets/CIFAR10.py
@@ -2,33 +2,40 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-class CIFAR10Dataset():
+
+class CIFAR10Dataset:
     def __init__(self, batch_size=256):
         super().__init__()
 
         self.batch_size = batch_size
 
         self.transform = transforms.Compose(
-                [transforms.ToTensor(),
-                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
 
         self.trainset = torchvision.datasets.CIFAR10(
-                root='./data', train=True, download=True, transform=self.transform)
+            root="./data", train=True, download=True, transform=self.transform
+        )
 
         self.trainloader = torch.utils.data.DataLoader(
-                self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=2)
-
+            self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=2
+        )
 
         self.testset = torchvision.datasets.CIFAR10(
-                root='./data', train=False, download=True, transform=self.transform)
+            root="./data", train=False, download=True, transform=self.transform
+        )
 
         self.testloader = torch.utils.data.DataLoader(
-                self.testset, batch_size=self.batch_size, shuffle=False, num_workers=2)
+            self.testset, batch_size=self.batch_size, shuffle=False, num_workers=2
+        )
 
     def channels(self):
         # image size = (3 * 32 * 32)
         return 3
-    
+
     def size(self):
         # image size = (3 * 32 * 32)
         return 32
