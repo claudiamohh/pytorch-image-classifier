3123f04b2291fac588b88e1f759657c79207190e
Lik Xun Yuan <likyuan@handshakes.com.sg>
2022-06-23 18:14:34 +0800

    Refactor training script

--- train.py
+++ train.py
@@ -9,27 +9,16 @@ import torchvision.transforms as transforms
 
 import os
 import argparse
-
+from tqdm import tqdm
 from models.LinearClassifier import Linear
 from models.CNNClassifier import CNN
 from datasets.CIFAR10 import CIFAR10Dataset
 from datasets.MNIST import MNISTDataset
 
-parser = argparse.ArgumentParser(description='Convolutional Neural Network Training')
-parser.add_argument('--lr', default=0.001, type=float, help='Learning rate')
-parser.add_argument('--epoch', default=10, type=int, help='Number of epoch')
-parser.add_argument('--model')
-parser.add_argument('--dataset')
-args = parser.parse_args()
-
-#Checking if GPU is available
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-print(device)
-
 
 def train (model, optimizer, criterion, trainloader, epochs):
     '''Training the model using the variables: model, optimizer, criterion, trainloader, number of epochs'''
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs), desc="Model training"):
         running_loss = 0.0
         for batch_idx, data in enumerate(trainloader, 0):
 
@@ -50,7 +39,6 @@ def train (model, optimizer, criterion, trainloader, epochs):
             running_loss += loss.item() #loss item of each batch 
         print(f'Loss of Epoch: {epoch} is {running_loss/len(trainloader)}')
 
-    print('Finished Training')
     return model 
 
 def evaluate_overall(model, testloader):
@@ -60,7 +48,7 @@ def evaluate_overall(model, testloader):
 
     #prediction for testing set
     with torch.no_grad():
-        for data in testloader:
+        for data in tqdm(testloader, desc="Evaluating on test set"):
             inputs, labels = data[0].to(device), data[1].to(device)
             outputs = model(inputs)     #calculate outputs by running inputs through the network
             _, predicted = torch.max(outputs.data, 1)
@@ -69,18 +57,33 @@ def evaluate_overall(model, testloader):
 
     print(f'Accuracy of the network on the test images: {100 * correct // total} %')
 
-def main():
+def main(args, device):
+
     dataset = MNISTDataset() if args.dataset == "mnist" else CIFAR10Dataset()
     channels = dataset.channels()
     size = dataset.size()
+
     train_loader, test_loader = dataset.get_loaders()
-    criterion = nn.CrossEntropyLoss()       #Loss function to calculate the difference between input and target
-    model = CNN(channels, use_mnist=(args.dataset == "mnist")) if args.model == "cnn" else Linear(channels, size)
+
+    use_mnist = (args.dataset == "mnist")
+
+    model = CNN(channels, use_mnist=use_mnist) if args.model == "cnn" else Linear(channels, size)
     model = model.to(device)
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)      #Adam optimizer 
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     model = train(model, optimizer, criterion, train_loader, args.epoch)
     evaluate_overall(model, test_loader)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Convolutional Neural Network Training')
+    parser.add_argument('--lr', default=0.001, type=float, help='Learning rate')
+    parser.add_argument('--epoch', default=10, type=int, help='Number of epoch')
+    parser.add_argument('--model')
+    parser.add_argument('--dataset')
+    args = parser.parse_args()
+
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+    main(args, device)
