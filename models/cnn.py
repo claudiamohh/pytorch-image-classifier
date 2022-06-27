fe96e4185893383b280ef8373ad3f51d67661dbd
Claudia <claudiamhch@gmail.com>
2022-06-28 01:09:08 +0800

    Add more functions to model

--- models/cnn.py
+++ models/cnn.py
@@ -90,4 +90,38 @@ class LightningCNN(pl.LightningModule):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    def evaluate(self, batch, stage=None):
+        x, y = batch
+        logits = self.forward(x)
 
+        loss = F.cross_entropy(logits, y.long())
+
+        preds = torch.argmax(logits, -1)
+        acc = accuracy(preds, y)
+        f1 = f1_score(preds, y, num_classes=10)
+
+        if stage:
+            self.log(f"{stage}_loss", loss, prog_bar=True) # show metrics in progress bar
+            self.log(f"{stage}_acc", acc, prog_bar=True)
+            self.log(f"{stage}_f1", f1, prog_bar=True)
+        
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.forward(x)
+        loss = F.cross_entropy(logits, y)
+        self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, "valid")
+    
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, "test")
+
+    def predict_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.forward(x)
+        return logits 
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=args.lr)
