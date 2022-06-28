25092e29cbd351e5983ce81f6cd2bad3e55e7581
Claudia <claudiamhch@gmail.com>
2022-06-28 10:52:00 +0800

    Add Lightning linear model

--- models/linear.py
+++ models/linear.py
@@ -2,7 +2,8 @@ import torch
 import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torchvision.models as models
+import pytorch_lightning as pl
 
 class Linear(nn.Module):
     def __init__(self, channels, size):
@@ -19,3 +20,55 @@ class Linear(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+class LightningLinear(pl.LightningModule):
+    def __init__(self, channels, size):
+        super().__init__()
+        self.fc1 = nn.Linear(
+                channels * size * size, 120
+        )      #input has to be image size to fit 
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self,x):
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x)) #activation function 
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x 
+    
+    def evaluate(self, batch, stage=None):
+        x, y = batch
+        logits = self.forward(x)
+
+        loss = F.cross_entropy(logits, y.long())
+
+        preds = torch.argmax(logits, -1)
+        acc = accuracy(preds, y)
+        f1 = f1_score(preds, y, num_classes=10)
+
+        if stage:
+            self.log(f"{stage}_loss", loss, prog_bar=True) # show metrics in progress bar
+            self.log(f"{stage}_acc", acc, prog_bar=True)
+            self.log(f"{stage}_f1", f1, prog_bar=True)
+        
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.forward(x)
+        loss = F.cross_entropy(logits, y)
+        self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, "valid")
+    
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, "test")
+
+    def predict_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.forward(x)
+        return logits 
+
+    def configure_optimizers(self, lr):
+        return torch.optim.Adam(self.parameters(), lr=lr)
